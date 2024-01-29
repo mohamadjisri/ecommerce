2f1d42c33b27c66138152446afec31ac540de86f
mohamadjisri <mohammadjisri@gmail.com>
2024-01-29 10:52:54 +0100

    34. Check Internet
    (28-33 PHP)

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,20 +1,22 @@
 import 'package:ecommerce/core/constant/routes.dart';
-import 'package:ecommerce/core/middleware/mymiddleware.dart';
+import 'package:ecommerce/test.dart';
 import 'package:ecommerce/view/screen/auth/forgetpassword/forgetpassword.dart';
-import 'package:ecommerce/view/screen/auth/login.dart';
 import 'package:ecommerce/view/screen/auth/forgetpassword/resetpassword.dart';
-import 'package:ecommerce/view/screen/auth/signup.dart';
 import 'package:ecommerce/view/screen/auth/forgetpassword/success_resetpassword.dart';
-import 'package:ecommerce/view/screen/auth/success_signup.dart';
 import 'package:ecommerce/view/screen/auth/forgetpassword/verifycode.dart';
+import 'package:ecommerce/view/screen/auth/login.dart';
+import 'package:ecommerce/view/screen/auth/signup.dart';
+import 'package:ecommerce/view/screen/auth/success_signup.dart';
 import 'package:ecommerce/view/screen/auth/verifycodesignup.dart';
-import 'package:ecommerce/view/screen/language.dart';
 import 'package:ecommerce/view/screen/onboarding.dart';
-import 'package:get/get_navigation/src/routes/get_route.dart';
+import 'package:get/get.dart';
 
 List<GetPage<dynamic>>? routes = [
-  GetPage(
-      name: "/", page: () => const Language(), middlewares: [MyMiddleWare()]),
+  // GetPage(name: "/", page: () => const Language() , middlewares: [
+  //   MyMiddleWare()
+  // ]),
+  GetPage(name: "/", page: () => const Test()),
+
   GetPage(name: AppRoute.login, page: () => const Login()),
   GetPage(name: AppRoute.signUp, page: () => const SignUp()),
   GetPage(name: AppRoute.forgetPassword, page: () => const ForgetPassword()),
