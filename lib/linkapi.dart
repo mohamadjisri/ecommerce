cb16ffa5db11713dabe609c5a8856fa9495fea16
mohamadjisri <mohammadjisri@gmail.com>
2024-01-31 11:04:09 +0100

    47. CheckEmail & ResetPassword (FrontEnd)
    46. CheckEmail & ResetPassword (BackEnd - PHP)

--- lib/linkapi.dart
+++ lib/linkapi.dart
@@ -7,4 +7,12 @@ class AppLink {
   static const String signUp = "$server/auth/signup.php";
   static const String login = "$server/auth/login.php";
   static const String verifycodessignup = "$server/auth/verifycode.php";
+
+// ======================= ForgetPassword ====================== //
+
+  static const String checkEmail = "$server/forgetpassword/checkemail.php";
+  static const String resetPassword =
+      "$server/forgetpassword/resetpassword.php";
+  static const String verifycodeforgetpassword =
+      "$server/forgetpassword/verifycode.php";
 }
